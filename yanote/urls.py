bac734c2fffd51940932b87fcb57f9e2f19e0b97
Zencat5888 <s.y.baranov@gmail.com>
2025-04-21 22:46:21 +0300

    Все тесты работают. Переписал под Dajngo 5.1

--- yanote/urls.py
+++ yanote/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import include, path
 from django.views.generic import CreateView
 
+from .views import logout_view
+
 urlpatterns = [
     path('', include('notes.urls')),
     path('admin/', admin.site.urls),
@@ -17,11 +19,16 @@ auth_urls = ([
     ),
     path(
         'logout/',
-        auth_views.LogoutView.as_view(
-            template_name='registration/logout.html'
-        ),
+        logout_view,
         name='logout',
     ),
+    # path(
+    #     'logout/',
+    #     auth_views.LogoutView.as_view(
+    #         template_name='registration/logout.html'
+    #     ),
+    #     name='logout',
+    # ),
     path(
         'signup/',
         CreateView.as_view(
---
+++ yanote/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import logout
+from django.shortcuts import render
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'registration/logout.html')
